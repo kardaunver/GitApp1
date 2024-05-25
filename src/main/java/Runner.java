0eb82a212a6bb231db3cd4d23f34d3db2e3e3599
kardaunver <kaanardaunver@gmail.com>
2024-05-25 20:47:56 +0300

    v4

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,9 +1,12 @@
 public class Runner {
     public static void main(String[] args) {
 
-    //versiyon 1
+        //versiyon 1
         System.out.println("Hello GIT");
         System.out.println("git add . : staging areaya alir.");
+
+        //versiyon 4
+        System.out.println("Hi");
 
     }
 }
